9d323566e38e30994d069d0513c7304b27751c8d
Vitaly <vitalian42@mail.ru>
2019-12-10 01:04:51 +0300

    Работающие клавиши пульта управления GSS, GST, SDS, SDT.

--- prog/SS/SS.asm
+++ prog/SS/SS.asm
@@ -10,7 +10,7 @@
 .def actual_device_statuses = R24 ;Отображает устройства, для
 							;которых уже выведено актуально состояние
 .def temp2 = R25	;Дополнительный временный буфер
-.def counter2 = R26 ;Дополнительный счетчик для циклов
+.def counter2 = R2 ;Дополнительный счетчик для циклов
 
 .def a_status = R1 ;Регистр хранения статусов устройств на порте A
 
@@ -63,6 +63,12 @@ INIT:
 	out DDRA, temp
 	clr temp
 	out PORTA, temp
+
+	ldi temp, 0b11110000
+	out DDRC, temp ;Инициализируем PC0-3 на вход, PC4-7 на выход
+	ldi temp, 0b00001111
+	out PORTC, temp ;Ставим 0 на пинах PC4-7 и
+					;подключаем подтягивающие резисторы на порты PC0-3
 	
 	;Настройка таймеров и прерываний
 	cli	;Запрещаем прерывания
@@ -99,9 +105,489 @@ skip_in:
 	;mov	temp, R1
 	;out 	PORTA, temp
 	rcall 	out_schedule
+	rcall 	check_klava
 	
 
+; Delay 800 000 cycles (0.1 секунды задержка
+; для уменьшение нагрузки на симуляцию в
+; протеусе при 8.0 MHz)
+
+    ldi  	r18, 5
+    ldi  	r19, 15
+    ldi  	r17, 242
+L1: dec  	r17
+    brne 	L1
+    dec  	r19
+    brne 	L1
+    dec  	r18
+    brne 	L1
+
+
 	rjmp 	main
+
+;#### ПРОЦЕДУРА ОПРОСА МАТРИЧНОЙ КЛАВИАТУРЫ ####
+check_klava:
+	ldi temp, 0
+	ldi temp, 0b00011111
+	out PORTC, temp
+
+	sbic PINC, 0
+	rcall SEVEN
+	sbic PINC, 1
+	rcall FOUR
+	sbic PINC, 2
+	rcall ONE
+	sbic PINC, 3
+	rcall ZERO
+
+	ldi temp, (1<<5)
+	out PORTC, temp
+
+	sbic PINC, 0
+	rcall EIGHT
+	sbic PINC, 1
+	rcall FIVE
+	sbic PINC, 2
+	rcall TWO
+	sbic PINC, 3
+	rcall SDS
+
+	ldi temp, (1<<6)
+	out PORTC, temp
+
+	sbic PINC, 0
+	rcall NINE
+	sbic PINC, 1
+	rcall SIX
+	sbic PINC, 2
+	rcall THREE
+	sbic PINC, 3
+	rcall SDT
+
+	ldi temp, (1<<7)
+	out PORTC, temp
+
+	sbic PINC, 0
+	rcall SSM
+	sbic PINC, 1
+	rcall STM
+	sbic PINC, 2
+	rcall GSS
+	sbic PINC, 3
+	rcall GST
+
+	ret
+
+
+SEVEN:
+	in temp, PORTA
+	ori temp, 0b01000000
+	out	PORTA, temp
+	ret
+
+FOUR:
+	in temp, PORTA
+	ori temp, 0b00001000
+	out	PORTA, temp
+	ret
+
+ONE:
+	in temp, PORTA
+	ori temp, 0b00000001
+	out	PORTA, temp
+	ret
+
+ZERO:
+	ldi temp, 0
+	out PORTA, temp
+	ret
+
+EIGHT:
+FIVE:
+TWO:
+SDS:
+	cli
+	sbic PINC, 3
+	rjmp SDS
+
+	ldi		XL, low(schedule_count)
+	ldi		XH, high(schedule_count)
+	ldi		temp, 16	;16 записей по умолчанию
+	st		X, temp		;(по 2 для каждого устройтсва
+
+	ldi		temp, 129	;1
+	st 		Y+, temp	;заголовок, определяющий устройства
+	ldi		temp, 12
+	st 		Y+, temp	;часы начала работы
+	ldi		temp, 0
+	st		Y+, temp	;минуты начала работы
+	ldi		temp, 0
+	st 		Y+, temp	;секунды начала работы
+
+	ldi		temp, 130	;2
+	st 		Y+, temp	;заголовок, определяющий устройства
+	ldi		temp, 12
+	st 		Y+, temp	;часы начала работы
+	ldi		temp, 0
+	st		Y+, temp	;минуты начала работы
+	ldi		temp, 0
+	st 		Y+, temp	;секунды начала работы
+	
+	ldi		temp, 131	;3
+	st 		Y+, temp	;заголовок, определяющий устройства
+	ldi		temp, 12
+	st 		Y+, temp	;часы начала работы
+	ldi		temp, 0
+	st		Y+, temp	;минуты начала работы
+	ldi		temp, 0
+	st 		Y+, temp	;секунды начала работы
+	
+	ldi		temp, 132	;4
+	st 		Y+, temp	;заголовок, определяющий устройства
+	ldi		temp, 12
+	st 		Y+, temp	;часы начала работы
+	ldi		temp, 0
+	st		Y+, temp	;минуты начала работы
+	ldi		temp, 0
+	st 		Y+, temp	;секунды начала работы
+	
+	ldi		temp, 133	;5
+	st 		Y+, temp	;заголовок, определяющий устройства
+	ldi		temp, 12
+	st 		Y+, temp	;часы начала работы
+	ldi		temp, 0
+	st		Y+, temp	;минуты начала работы
+	ldi		temp, 0
+	st 		Y+, temp	;секунды начала работы
+	
+	ldi		temp, 134	;6
+	st 		Y+, temp	;заголовок, определяющий устройства
+	ldi		temp, 12
+	st 		Y+, temp	;часы начала работы
+	ldi		temp, 0
+	st		Y+, temp	;минуты начала работы
+	ldi		temp, 0
+	st 		Y+, temp	;секунды начала работы	
+		
+	ldi		temp, 135	;7
+	st 		Y+, temp	;заголовок, определяющий устройства
+	ldi		temp, 12
+	st 		Y+, temp	;часы начала работы
+	ldi		temp, 0
+	st		Y+, temp	;минуты начала работы
+	ldi		temp, 0
+	st 		Y+, temp	;секунды начала работы
+	
+	ldi		temp, 136	;8
+	st 		Y+, temp	;заголовок, определяющий устройства
+	ldi		temp, 12
+	st 		Y+, temp	;часы начала работы
+	ldi		temp, 0
+	st		Y+, temp	;минуты начала работы
+	ldi		temp, 0
+	st 		Y+, temp	;секунды начала работы
+
+
+	ldi		temp, 1		;1
+	st 		Y+, temp	;заголовок, определяющий устройства
+	ldi		temp, 18
+	st 		Y+, temp	;часы начала работы
+	ldi		temp, 0
+	st		Y+, temp	;минуты начала работы
+	ldi		temp, 0
+	st 		Y+, temp	;секунды начала работы
+
+	ldi		temp, 2		;2
+	st 		Y+, temp	;заголовок, определяющий устройства
+	ldi		temp, 18
+	st 		Y+, temp	;часы начала работы
+	ldi		temp, 0
+	st		Y+, temp	;минуты начала работы
+	ldi		temp, 0
+	st 		Y+, temp	;секунды начала работы
+	
+	ldi		temp, 3		;3
+	st 		Y+, temp	;заголовок, определяющий устройства
+	ldi		temp, 18
+	st 		Y+, temp	;часы начала работы
+	ldi		temp, 0
+	st		Y+, temp	;минуты начала работы
+	ldi		temp, 0
+	st 		Y+, temp	;секунды начала работы
+	
+	ldi		temp, 4		;4
+	st 		Y+, temp	;заголовок, определяющий устройства
+	ldi		temp, 18
+	st 		Y+, temp	;часы начала работы
+	ldi		temp, 0
+	st		Y+, temp	;минуты начала работы
+	ldi		temp, 0
+	st 		Y+, temp	;секунды начала работы
+	
+	ldi		temp, 5		;5
+	st 		Y+, temp	;заголовок, определяющий устройства
+	ldi		temp, 18
+	st 		Y+, temp	;часы начала работы
+	ldi		temp, 0
+	st		Y+, temp	;минуты начала работы
+	ldi		temp, 0
+	st 		Y+, temp	;секунды начала работы
+	
+	ldi		temp, 6		;6
+	st 		Y+, temp	;заголовок, определяющий устройства
+	ldi		temp, 18
+	st 		Y+, temp	;часы начала работы
+	ldi		temp, 0
+	st		Y+, temp	;минуты начала работы
+	ldi		temp, 0
+	st 		Y+, temp	;секунды начала работы	
+		
+	ldi		temp, 7		;7
+	st 		Y+, temp	;заголовок, определяющий устройства
+	ldi		temp, 18
+	st 		Y+, temp	;часы начала работы
+	ldi		temp, 0
+	st		Y+, temp	;минуты начала работы
+	ldi		temp, 0
+	st 		Y+, temp	;секунды начала работы
+	
+	ldi		temp, 8		;8
+	st 		Y+, temp	;заголовок, определяющий устройства
+	ldi		temp, 18
+	st 		Y+, temp	;часы начала работы
+	ldi		temp, 0
+	st		Y+, temp	;минуты начала работы
+	ldi		temp, 0
+	st 		Y+, temp	;секунды начала работы
+
+	ldi		temp,'O'
+	rcall	out_com
+	ldi		temp,'N'
+	rcall	out_com
+	ldi		temp,' '
+	rcall	out_com
+	ldi		temp,'i'
+	rcall	out_com
+	ldi		temp,'n'
+	rcall	out_com
+	ldi		temp,' '
+	rcall	out_com
+	ldi		temp,'1'
+	rcall	out_com
+	ldi		temp,'2'
+	rcall	out_com
+	ldi		temp,':'
+	rcall	out_com
+	ldi		temp,'0'
+	rcall	out_com
+	ldi		temp,'0'
+	rcall	out_com
+	ldi		temp,' '
+	rcall	out_com
+	ldi		temp,'a'
+	rcall	out_com
+	ldi		temp,'n'
+	rcall	out_com
+	ldi		temp,'d'
+	rcall	out_com
+	ldi		temp,' '
+	rcall	out_com
+	ldi		temp,'o'
+	rcall	out_com
+	ldi		temp,'f'
+	rcall	out_com
+	ldi		temp,'f'
+	rcall	out_com
+	ldi		temp,' '
+	rcall	out_com
+	ldi		temp,'i'
+	rcall	out_com
+	ldi		temp,'n'
+	rcall	out_com
+	ldi		temp,' '
+	rcall	out_com
+	ldi		temp,'1'
+	rcall	out_com
+	ldi		temp,'8'
+	rcall	out_com
+	ldi		temp,':'
+	rcall	out_com
+	ldi		temp,'0'
+	rcall	out_com
+	ldi		temp,'0'
+	rcall	out_com
+	ldi		temp,0x0A	;"ПЕРЕВОД СТРОКИ" Перевод курсора на строку ниже
+	rcall	out_com
+	ldi		temp,0x0D	;"ВОЗВРАТ КАРЕТКИ" Переход на начало текущей строки
+	rcall	out_com
+
+	sei
+	ret
+NINE:
+SIX:
+THREE:
+	ret
+SDT:
+	cli
+	sbic PINC, 3
+	rjmp SDT
+
+	ldi time_seconds, 0
+	ldi time_minutes, 30
+	ldi time_hours, 12
+
+	ldi		temp,'S'
+	rcall	out_com
+	ldi		temp,'e'
+	rcall	out_com
+	ldi		temp,'t'
+	rcall	out_com
+	ldi		temp,'e'
+	rcall	out_com
+	ldi		temp,'d'
+	rcall	out_com
+	ldi		temp,' '
+	rcall	out_com
+	ldi		temp,'d'
+	rcall	out_com
+	ldi		temp,'e'
+	rcall	out_com
+	ldi		temp,'f'
+	rcall	out_com
+	ldi		temp,'a'
+	rcall	out_com
+	ldi		temp,'u'
+	rcall	out_com
+	ldi		temp,'t'
+	rcall	out_com
+	ldi		temp,' '
+	rcall	out_com
+	ldi		temp,'t'
+	rcall	out_com
+	ldi		temp,'i'
+	rcall	out_com
+	ldi		temp,'m'
+	rcall	out_com
+	ldi		temp,'e'
+	rcall	out_com
+	ldi		temp,':'
+	rcall	out_com
+	ldi		temp,' '
+	rcall	out_com
+	ldi		temp,'1'
+	rcall	out_com
+	ldi		temp,'2'
+	rcall	out_com
+	ldi		temp,':'
+	rcall	out_com
+	ldi		temp,'3'
+	rcall	out_com
+	ldi		temp,'0'
+	rcall	out_com
+	ldi		temp,':'
+	rcall	out_com
+	ldi		temp,'0'
+	rcall	out_com
+	ldi		temp,'0'
+	rcall	out_com
+	ldi		temp,0x0A	;"ПЕРЕВОД СТРОКИ" Перевод курсора на строку ниже
+	rcall	out_com
+	ldi		temp,0x0D	;"ВОЗВРАТ КАРЕТКИ" Переход на начало текущей строки
+	rcall	out_com
+
+	sei
+	ret
+SSM:
+STM:
+	ret
+GSS:
+	cli
+	sbic PINC, 2
+	rjmp GSS
+
+	ldi		temp,'N'
+	rcall	out_com
+	ldi		temp,'e'
+	rcall	out_com
+	ldi		temp,'e'
+	rcall	out_com
+	ldi		temp,'d'
+	rcall	out_com
+	ldi		temp,' '
+	rcall	out_com
+	ldi		temp,'n'
+	rcall	out_com
+	ldi		temp,'e'
+	rcall	out_com
+	ldi		temp,'w'
+	rcall	out_com
+	ldi		temp,' '
+	rcall	out_com
+	ldi		temp,'s'
+	rcall	out_com
+	ldi		temp,'c'
+	rcall	out_com
+	ldi		temp,'h'
+	rcall	out_com
+	ldi		temp,'e'
+	rcall	out_com
+	ldi		temp,'d'
+	rcall	out_com
+	ldi		temp,'u'
+	rcall	out_com
+	ldi		temp,'l'
+	rcall	out_com
+	ldi		temp,'e'
+	rcall	out_com
+	ldi		temp,0x0A	;"ПЕРЕВОД СТРОКИ" Перевод курсора на строку ниже
+	rcall	out_com
+	ldi		temp,0x0D	;"ВОЗВРАТ КАРЕТКИ" Переход на начало текущей строки
+	rcall	out_com
+	sei
+	ret
+GST:
+	cli
+	sbic PINC, 3
+	rjmp GST
+
+	ldi		temp,'N'
+	rcall	out_com
+	ldi		temp,'e'
+	rcall	out_com
+	ldi		temp,'e'
+	rcall	out_com
+	ldi		temp,'d'
+	rcall	out_com
+	ldi		temp,' '
+	rcall	out_com
+	ldi		temp,'a'
+	rcall	out_com
+	ldi		temp,'c'
+	rcall	out_com
+	ldi		temp,'t'
+	rcall	out_com
+	ldi		temp,'u'
+	rcall	out_com
+	ldi		temp,'a'
+	rcall	out_com
+	ldi		temp,'l'
+	rcall	out_com
+	ldi		temp,' '
+	rcall	out_com
+	ldi		temp,'t'
+	rcall	out_com
+	ldi		temp,'i'
+	rcall	out_com
+	ldi		temp,'m'
+	rcall	out_com
+	ldi		temp,'e'
+	rcall	out_com
+	ldi		temp,0x0A	;"ПЕРЕВОД СТРОКИ" Перевод курсора на строку ниже
+	rcall	out_com
+	ldi		temp,0x0D	;"ВОЗВРАТ КАРЕТКИ" Переход на начало текущей строки
+	rcall	out_com
+	sei
+	ret
 
 
 ;#### ПРОЦЕДУРА ОБНОВЛЕНИЯ СТАТУСА УСТРОЙСТВ ####
@@ -210,7 +696,7 @@ end_out_schedule:
 ;#### ПРОЦЕДУРА УСТАНОВКИ БИТА В ПЕРЕМЕННОЙ temp2 ####
 set_bit_temp2:	
 	ldi 	temp2, 1
-	ldi 	counter2, 1
+	mov 	counter2, temp2
 	cp 		counter2, temp
 	breq	set_bit_end	
 
