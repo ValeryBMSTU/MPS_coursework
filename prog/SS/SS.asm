507ebaba14f4ba1675b6eaa917bba70b219f5e0f
Vitaly <vitalian42@mail.ru>
2019-12-29 16:15:17 +0300

    Добавлен ручной ввод времени и прерывание по переполнению Таймера T1

--- prog/SS/SS.asm
+++ prog/SS/SS.asm
@@ -6,7 +6,7 @@
 .def on_off = R18 	;Признак того, что нужно выключить (1)
 				  	;или выключить (0) устройство 
 .def device_number = R19 ;Номер устройства, которое
-						 ;необзодимо включить/выключить
+						 ;необходимо включить/выключить
 .def actual_device_statuses = R24 ;Отображает устройства, для
 							;которых уже выведено актуально состояние
 .def temp2 = R25	;Дополнительный временный буфер
@@ -17,6 +17,13 @@
 .def force_devices = R4 ;Регистр устройств, запущенных в принудительном режиме
 .def ascii_numbers_start = R5
 .def byte_to_send = R6 ;Регистр, хранящий биты для передачи на дисплей
+
+.def inp_hours_h = R7
+.def inp_hours_l = R8
+.def inp_minutes_h = R9
+.def inp_minutes_l = R10
+.def inp_seconds_h = R11
+.def inp_seconds_l = R12
 
 
 .def time_extra = R20	;Дополнительный регистр времени 1
@@ -38,7 +45,8 @@ schedule_start:	.byte 1
 .cseg
 .org $000
 	rjmp INIT
-.org $007
+.org $006
+	rjmp TIME1_OVER
 	rjmp TIME0_OVER
 
 
@@ -179,21 +187,42 @@ INIT:
 	;Настройка таймеров и прерываний
 	cli	;Запрещаем прерывания
 
-	ldi temp, (1<<TOIE0) ;Разрешить прерывание по переполнению счетчика T0
+	ldi temp, (1<<TOIE0|1<<TOIE1) ;Разрешить прерывание по переполнению счетчика T0 и T1
 	out TIMSK, temp
 	
-	ldi temp, 6	;Установка начального
-	out TCNT0, temp ;значения счетчика T0 (при 9 МГц)
-	ldi time_extra, 131 ;Установка начального в дополнительный регистр времени
+	ldi temp, 0	;Установка начального
+	out TCNT0, temp ;значения счетчика T0 (при 8 МГц)
+
+
+	; Установка начального значения счетчика T1
+	ldi temp, 0b10000101
+	out TCNT1H, temp     
+	ldi temp, 0b11101110
+	out TCNT1L, temp
 
 	ldi time_seconds, 0
 	ldi time_minutes, 0
 	ldi time_hours, 0
 
-	ldi temp, (1<<CS02|0<<CS01|0<<CS00) ;Предделитель частоты T0 равен 256
+	ldi temp, (1<<CS02|0<<CS01|1<<CS00) ;Предделитель частоты T0 равен 1024
 	out TCCR0, temp						;Запуск счетчика T0
-	
+	ldi temp, (1<<CS12|0<<CS11|0<<CS10) ;Предделитель частоты T1 равен 256
+	out TCCR1B, temp					;Запуск счетчика T1
+
 	sei	;Разрешение прерываний
+
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+
 
 MAIN:
 	sbis	UCSRA,RXC	;Ожидание, когда бит RXC будет установлен в 1 
@@ -212,7 +241,6 @@ skip_in:
 	;out 	PORTA, temp
 	rcall 	out_schedule
 	rcall 	check_klava
-	rcall   display
 	rcall 	DELAY
 
 	rjmp 	main	
@@ -225,14 +253,14 @@ DELAY:
 ; для уменьшение нагрузки на симуляцию в
 ; протеусе при 8.0 MHz)
 
-    ldi  	r18, 5
-    ldi  	r19, 15
+    ldi  	r19, 5
+    ldi  	r18, 15
     ldi  	r17, 242
 L1: dec  	r17
     brne 	L1
-    dec  	r19
-    brne 	L1
     dec  	r18
+    brne 	L1
+    dec  	r19
     brne 	L1
 	ret
 
@@ -251,112 +279,6 @@ L2: dec  r19
     brne L2
 	ret
 
-
-;#### ПРОЦЕДУРА ОТОБРАЖЕНИЯ ВРЕМЕНИ НА ДИСПЛЕЕ ####
-display:
-
-	; Очистка дисплея
-	rcall 	SET_COMMAND_MODE
-	ldi 	temp,0b00000001
-	mov		byte_to_send, temp
-	rcall	SEND_BYTE
-
-	; Сдвигание курсора вправо на одну позицию
-	rcall 	SET_COMMAND_MODE
-	ldi 	temp,0b00010100
-	mov		byte_to_send, temp
-	rcall	SEND_BYTE
-	
-	; Заносим код начала цифр в ascii
-	ldi 	temp, 0x30
-	mov 	ascii_numbers_start, temp
-
-	; Заносим в регистр temp текущее время в часах
-	clr 	temp2
-	mov 	temp, time_hours
-
-	; Устанавливаем режим передачи символов
-	rcall 	SET_DATA_MODE
-	rcall 	LOW_DELAY
-
-hours_cicle: ; Считаем часы
-	cpi 	temp, 10.
-	brlo 	out_hours
-	inc 	temp2
-	subi 	temp, 10
-	rjmp 	hours_cicle
-
-out_hours:	; Выводим часы
-	; Выводим старшую цифру
-	add 	temp2, ascii_numbers_start
-	mov		byte_to_send, temp2
-	rcall	SEND_BYTE
-	; Выводим младшую цифру
-`	add 	temp, ascii_numbers_start
-	mov		byte_to_send, temp
-	rcall	SEND_BYTE
-
-	; Сдвигание курсора вправо на одну позицию
-	rcall 	SET_COMMAND_MODE
-	ldi 	temp,0b00010100
-	mov		byte_to_send, temp
-	rcall	SEND_BYTE
-
-	; Заносим в регистр temp текущее время в часах
-	clr 	temp2
-	mov 	temp, time_minutes
-
-	; Устанавливаем режим передачи символов
-	rcall 	SET_DATA_MODE
-
-minutes_cicle:
-	cpi 	temp, 10.
-	brlo 	out_minutes
-	inc 	temp2
-	subi 	temp, 10
-	rjmp 	minutes_cicle
-
-out_minutes: ; Выводим минуты
-	; Выводим старшую цифру
-	add 	temp2, ascii_numbers_start
-	mov		byte_to_send, temp2
-	rcall	SEND_BYTE
-	; Выводим младшую цифру
-`	add 	temp, ascii_numbers_start
-	mov		byte_to_send, temp
-	rcall	SEND_BYTE
-
-	; Сдвигание курсора вправо на одну позицию
-	rcall 	SET_COMMAND_MODE
-	ldi 	temp,0b00010100
-	mov		byte_to_send, temp
-	rcall	SEND_BYTE
-
-	; Заносим в регистр temp текущее время в часах
-	clr 	temp2
-	mov 	temp, time_seconds
-
-	; Устанавливаем режим передачи символов
-	rcall 	SET_DATA_MODE
-
-seconds_cicle:
-	cpi 	temp, 10.
-	brlo 	out_seconds
-	inc 	temp2
-	subi 	temp, 10
-	rjmp 	seconds_cicle
-
-out_seconds:
-	; Выводим старшую цифру
-	add 	temp2, ascii_numbers_start
-	mov		byte_to_send, temp2
-	rcall	SEND_BYTE
-	; Выводим младшую цифру
-`	add 	temp, ascii_numbers_start
-	mov		byte_to_send, temp
-	rcall	SEND_BYTE
-
-	ret
 
 ;#### ПРОЦЕДУРА ОТПРАВКИ БАЙТА НА ДИСПЛЕЙ ####
 SEND_BYTE:
@@ -422,7 +344,7 @@ check_klava:
 	out PORTC, temp
 
 	sbic PINC, 3
-	rcall SDT
+	rcall STF
 
 	ldi temp, (1<<7)
 	out PORTC, temp
@@ -1113,6 +1035,299 @@ GST:
 
 	ret
 
+STF:
+	sbic PINC, 3
+	rjmp STF
+
+	ldi temp2, 0
+
+stf_cicle:
+
+	ldi temp, (1<<4)
+	out PORTC, temp
+
+	sbic PINC, 0
+	rcall INP_SEVEN
+	sbic PINC, 1
+	rcall INP_FOUR
+	sbic PINC, 2
+	rcall INP_ONE
+	sbic PINC, 3
+	rcall INP_ZERO
+
+	ldi temp, (1<<5)
+	out PORTC, temp
+
+	sbic PINC, 0
+	rcall INP_EIGHT
+	sbic PINC, 1
+	rcall INP_FIVE
+	sbic PINC, 2
+	rcall INP_TWO
+
+	ldi temp, (1<<6)
+	out PORTC, temp
+
+	sbic PINC, 0
+	rcall INP_NINE
+	sbic PINC, 1
+	rcall INP_SIX
+	sbic PINC, 2
+	rcall INP_THREE
+
+	rcall 	DELAY
+
+	cpi 	temp2, 6
+	brlo	stf_cicle
+
+	rcall OUT_INP_TIME
+
+	ret
+
+INP_NINE:
+	sbic PINC, 0
+	rjmp INP_NINE
+
+	cpi	temp2, 0
+	breq end_inp_nine
+	cpi temp2, 2
+	breq end_inp_nine
+	cpi temp2, 4
+	breq end_inp_nine
+
+	ldi temp, 9
+	rcall INP_PUSH
+	inc temp2
+
+end_inp_nine:
+	ret
+
+
+INP_EIGHT:
+	sbic PINC, 0
+	rjmp INP_EIGHT
+
+	cpi	temp2, 0
+	breq end_inp_eight
+	cpi temp2, 2
+	breq end_inp_eight
+	cpi temp2, 4
+	breq end_inp_eight
+
+	ldi temp, 8
+	rcall INP_PUSH
+	inc temp2
+
+end_inp_eight:
+	ret
+
+
+
+INP_SEVEN:
+	sbic PINC, 0
+	rjmp INP_SEVEN
+
+	cpi	temp2, 0
+	breq end_inp_seven
+	cpi temp2, 2
+	breq end_inp_seven
+	cpi temp2, 4
+	breq end_inp_seven
+
+	ldi temp, 7
+	rcall INP_PUSH
+	inc temp2
+
+end_inp_seven:
+	ret
+
+
+
+INP_SIX:
+	sbic PINC, 1
+	rjmp INP_SIX
+
+	cpi	temp2, 0
+	breq end_inp_six
+	cpi temp2, 2
+	breq end_inp_six
+	cpi temp2, 4
+	breq end_inp_six
+
+	ldi temp, 6
+	rcall INP_PUSH
+	inc temp2
+
+end_inp_six:
+	ret
+
+
+
+INP_FIVE:
+	sbic PINC, 1
+	rjmp INP_FIVE
+
+	cpi	temp2, 0
+	breq end_inp_five
+
+
+	ldi temp, 5
+	rcall INP_PUSH
+	inc temp2
+
+end_inp_five:
+	ret
+
+
+
+INP_FOUR:
+	sbic PINC, 1
+	rjmp INP_FOUR
+
+	cpi	temp2, 0
+	breq end_inp_foure
+
+
+	ldi temp, 4
+	rcall INP_PUSH
+	inc temp2
+
+end_inp_foure:
+	ret
+
+
+
+INP_THREE:
+	sbic PINC, 2
+	rjmp INP_THREE
+
+	cpi	temp2, 0
+	breq end_inp_three
+
+
+	ldi temp, 3
+	rcall INP_PUSH
+	inc temp2
+
+end_inp_three:
+	ret
+
+
+
+INP_TWO:
+	sbic PINC, 2
+	rjmp INP_TWO
+
+	ldi temp, 2
+	rcall INP_PUSH
+	inc temp2
+
+end_inp_two:
+	ret
+
+
+
+INP_ONE:
+	sbic PINC, 2
+	rjmp INP_ONE
+
+	ldi temp, 1
+	rcall INP_PUSH
+	inc temp2
+
+end_inp_one:
+	ret
+
+
+
+INP_ZERO:
+	sbic PINC, 3
+	rjmp INP_ZERO
+
+	ldi temp, 0
+	rcall INP_PUSH
+	inc temp2
+
+end_inp_zero:
+	ret
+
+INP_PUSH:
+	cpi temp2, 0
+	brne next_inp1
+	mov	inp_hours_h, temp
+	rjmp inp_push_end
+next_inp1:
+	cpi temp2, 1
+	brne next_inp2
+	mov	inp_hours_l, temp
+	rjmp inp_push_end
+next_inp2:
+	cpi temp2, 2
+	brne next_inp3
+	mov	inp_minutes_h, temp
+	rjmp inp_push_end
+next_inp3:
+	cpi temp2, 3
+	brne next_inp4
+	mov	inp_minutes_l, temp
+	rjmp inp_push_end
+next_inp4:
+	cpi temp2, 4
+	brne next_inp5
+	mov	inp_seconds_h, temp
+	rjmp inp_push_end
+next_inp5:
+	cpi temp2, 5
+	brne inp_push_end
+	mov	inp_seconds_l, temp
+	rjmp inp_push_end
+
+inp_push_end:
+	ret
+
+
+OUT_INP_TIME:
+	clr time_seconds
+	clr time_minutes
+	clr time_hours
+
+	ldi temp2, 10
+
+	mov temp, inp_seconds_l
+	add time_seconds, temp
+	mov temp, inp_seconds_h
+	cpi temp, 0
+cicle_out_inp_seconds:
+	breq	next_out_inp_minutes
+	add		time_seconds, temp2
+	dec		temp
+	rjmp	cicle_out_inp_seconds
+
+next_out_inp_minutes:
+	mov temp, inp_minutes_l
+	add time_minutes, temp
+	mov temp, inp_minutes_h
+	cpi temp, 0
+cicle_out_inp_minutes:
+	breq	next_out_inp_hours
+	add		time_minutes, temp2
+	dec		temp
+	rjmp	cicle_out_inp_minutes
+
+next_out_inp_hours:
+	mov temp, inp_hours_l
+	add time_hours, temp
+	mov temp, inp_hours_h
+	cpi temp, 0
+cicle_out_inp_hours:
+	breq	end_out_inp
+	add		time_hours, temp2
+	dec		temp
+	rjmp	cicle_out_inp_hours
+
+end_out_inp:
+	ret
+	
 
 ;#### ПРОЦЕДУРА ОБНОВЛЕНИЯ СТАТУСА УСТРОЙСТВ ####
 out_schedule:
@@ -1139,13 +1354,6 @@ next_time:
 
 	andi	temp, 0b00001111	;Определяем номер устройства
 	mov		device_number, temp	;которое включаем/выключаем
-	
-	;mov 	temp, device_number ;Получаем нужный бит
-	;rcall 	set_bit_temp2		;устройства в temp2
-	;and		temp2, actual_device_statuses ;Проверяем актуален ли
-										  ;статус устройства
-	;cpi		temp2, 0  ;Если не равно нулю, то 
-	;brne	skip_time ;статус устройства актуален
 
 	ld		temp, Y+
 	cp		temp, time_hours ;Сравнение по часам
@@ -1300,39 +1508,297 @@ in_com:
 
 ;#### РАЗЛИЧНЫЕ ПРЕРЫВАНИЯ ####
 
-TIME0_OVER:
+TIME1_OVER:
 	push temp ; Сохранение регистра temp
+	push temp2
 
-	ldi temp, 6	;Установка начала отчета 
-	out TCNT0, temp ;для счетчика T0 (при 9 МГц)
+	; Установка начального значения счетчика T1
+	ldi temp, 0b10000101
+	out TCNT1H, temp     
+	ldi temp, 0b11101110
+	out TCNT1L, temp
 
-	inc time_extra
-	cpi time_extra, 0
-	brne time0_continue
-
-	ldi time_extra, 131 ;Установка начального в дополнительный регистр времени
 	inc time_seconds
 	cpi time_seconds, 60
-	brne time0_continue
+	brne time1_continue
 
 	ldi time_seconds, 0
 	inc time_minutes
 	cpi time_minutes, 60
-	brne time0_continue
+	brne time1_continue
 	
 	ldi time_minutes, 0
 	inc time_hours
 	cpi time_hours, 24
-	brne time0_continue
+	brne time1_continue
 
 	ldi time_hours, 0
 
-time0_continue:
+time1_continue:
+	pop temp2
 	pop temp
 
 	reti
 
+TIME0_OVER:
+	push R16
+	push R17
+	push R18
+	push R19
+	push R25
+
+	; Очистка дисплея
+	rcall 	SET_COMMAND_MODE
+	ldi 	temp,0b00000001
+	mov		byte_to_send, temp
+	rcall	SEND_BYTE
+
+	; Сдвигание курсора вправо на одну позицию
+	rcall 	SET_COMMAND_MODE
+	ldi 	temp,0b00010100
+	mov		byte_to_send, temp
+	rcall	SEND_BYTE
+
+	; Устанавливаем режим передачи символов
+	rcall 	SET_DATA_MODE
+	rcall 	LOW_DELAY
+
+	ldi 	temp, 'T'
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	ldi 	temp, 'I'
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	ldi 	temp, 'M'
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	ldi 	temp, 'E'
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	ldi 	temp, ':'
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
 	
+	; Сдвигание курсора вправо на одну позицию
+	rcall 	SET_COMMAND_MODE
+	ldi 	temp,0b00010100
+	mov		byte_to_send, temp
+	rcall	SEND_BYTE
+
+	; Заносим код начала цифр в ascii
+	ldi 	temp, 0x30
+	mov 	ascii_numbers_start, temp
+
+	; Заносим в регистр temp текущее время в часах
+	clr 	temp2
+	mov 	temp, time_hours
+
+	; Устанавливаем режим передачи символов
+	rcall 	SET_DATA_MODE
+	rcall 	LOW_DELAY
+
+hours_cicle: ; Считаем часы
+	cpi 	temp, 10.
+	brlo 	out_hours
+	inc 	temp2
+	subi 	temp, 10
+	rjmp 	hours_cicle
+
+out_hours:	; Выводим часы
+	; Выводим старшую цифру
+	add 	temp2, ascii_numbers_start
+	mov		byte_to_send, temp2
+	rcall	SEND_BYTE
+	; Выводим младшую цифру
+`	add 	temp, ascii_numbers_start
+	mov		byte_to_send, temp
+	rcall	SEND_BYTE
+
+	ldi 	temp, ':'
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	; Заносим в регистр temp текущее время в часах
+	clr 	temp2
+	mov 	temp, time_minutes
+
+minutes_cicle:
+	cpi 	temp, 10.
+	brlo 	out_minutes
+	inc 	temp2
+	subi 	temp, 10
+	rjmp 	minutes_cicle
+
+out_minutes: ; Выводим минуты
+	; Выводим старшую цифру
+	add 	temp2, ascii_numbers_start
+	mov		byte_to_send, temp2
+	rcall	SEND_BYTE
+	; Выводим младшую цифру
+`	add 	temp, ascii_numbers_start
+	mov		byte_to_send, temp
+	rcall	SEND_BYTE
+
+	ldi 	temp, ':'
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	; Заносим в регистр temp текущее время в часах
+	clr 	temp2
+	mov 	temp, time_seconds
+
+seconds_cicle:
+	cpi 	temp, 10.
+	brlo 	out_seconds
+	inc 	temp2
+	subi 	temp, 10
+	rjmp 	seconds_cicle
+
+out_seconds:
+	; Выводим старшую цифру
+	add 	temp2, ascii_numbers_start
+	mov		byte_to_send, temp2
+	rcall	SEND_BYTE
+	; Выводим младшую цифру
+`	add 	temp, ascii_numbers_start
+	mov		byte_to_send, temp
+	rcall	SEND_BYTE
+
+	; Перемещаем курсор в левый нижний угол
+	rcall 	SET_COMMAND_MODE
+	ldi 	temp,0b11000000 ; 0xC0 - адрес левого нижнего угла дисплея
+	mov		byte_to_send, temp
+	rcall	SEND_BYTE
+	
+	; Устанавливаем режим передачи символов
+	rcall 	SET_DATA_MODE
+
+	ldi 	temp, 'D'
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	ldi 	temp, 'E'
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	ldi 	temp, 'V'
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	ldi 	temp, 'I'
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	ldi 	temp, 'C'
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	ldi 	temp, 'E'
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	ldi 	temp, 'S'
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	ldi 	temp, ':'
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	; Устанавливаем режим передачи символов
+	rcall 	SET_DATA_MODE
+
+	sbic	PINA, 0
+	rjmp	dev0_is_1
+	ldi 	temp, '0'
+	rjmp	dev0_out
+dev0_is_1:
+	ldi		temp, '1'
+dev0_out:
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+	
+	sbic	PINA, 1
+	rjmp	dev1_is_1
+	ldi 	temp, '0'
+	rjmp	dev1_out
+dev1_is_1:
+	ldi		temp, '1'
+dev1_out:
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	sbic	PINA, 2
+	rjmp	dev2_is_1
+	ldi 	temp, '0'
+	rjmp	dev2_out
+dev2_is_1:
+	ldi		temp, '1'
+dev2_out:
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	sbic	PINA, 3
+	rjmp	dev3_is_1
+	ldi 	temp, '0'
+	rjmp	dev3_out
+dev3_is_1:
+	ldi		temp, '1'
+dev3_out:
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	sbic	PINA, 4
+	rjmp	dev4_is_1
+	ldi 	temp, '0'
+	rjmp	dev4_out
+dev4_is_1:
+	ldi		temp, '1'
+dev4_out:
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	sbic	PINA, 5
+	rjmp	dev5_is_1
+	ldi 	temp, '0'
+	rjmp	dev5_out
+dev5_is_1:
+	ldi		temp, '1'
+dev5_out:
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	sbic	PINA, 6
+	rjmp	dev6_is_1
+	ldi 	temp, '0'
+	rjmp	dev6_out
+dev6_is_1:
+	ldi		temp, '1'
+dev6_out:
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	sbic	PINA, 7
+	rjmp	dev7_is_1
+	ldi 	temp, '0'
+	rjmp	dev7_out
+dev7_is_1:
+	ldi		temp, '1'
+dev7_out:
+	mov 	byte_to_send, temp
+	rcall	SEND_BYTE
+
+	pop R25
+	pop R19
+	pop R18
+	pop R17
+	pop R16
+
+	reti
 
 
 
